ce500f70877fc1acdcf1f74b159d041b0c4f0b86
Adam Bourn <abourn@uw.edu>
2017-09-14 15:45:48 -0700

    start working on projects section, test styling, import bootstrap

--- src/App.js
+++ src/App.js
@@ -7,26 +7,46 @@ class App extends Component {
     return (
       <div className="App">
         <section className="about">
-        <div>
-          <header>
-            <h1>Adam Bourn</h1>
-            <nav>
-              <a>About</a>
-              <a href="#projectsSection">Projects</a>
-              <a>Resume</a>
-            </nav>
-          </header>
-        </div>
-        <div className="aboutTextContainer">
-          <p>Senior | Informatics | University of Washington</p>
-        </div>
-        <div className="arrowContainer">
-          <a><i className="fa fa-angle-down fa-3x faa-passing animated"></i></a>
-        </div>
+          <div>
+            <header>
+              <h1>Adam Bourn</h1>
+              <nav>
+                <a>About</a>
+                <a href="#projectsSection">Projects</a>
+                <a href="https://drive.google.com/file/d/0B8Mc9nigpockVUR1QkE3UVcyX00/view?usp=sharing" target="_blank">Resume</a>
+              </nav>
+            </header>
+          </div>
+          <div className="aboutTextContainer">
+            <p>Senior | Informatics | University of Washington</p>
+          </div>
+          <div className="arrowContainer">
+            <a><i className="fa fa-angle-down fa-3x faa-passing animated"></i></a>
+          </div>
         </section>
-        <section name="projectsSection" className="projcects">
+        <section name="projectsSection" className="projects">
+          <h2>Projects</h2>
+          <Project />
         </section>
       </div>
+    );
+  }
+}
+
+class Project extends Component {
+  render() {
+    return (
+      <div className="flex-container container">
+        <div className="flex-column">
+          <div className="content">
+            <div className="image"></div>
+            <div className="textContent">
+              <h3>Note Taking</h3>
+              <p>Having trouble remembering those little details? Take a Pencil with you and jot them down wherever you go.  IF you make a mistake, erase it and start over.</p>
+          </div>
+        </div>
+      </div>
+    </div>
     );
   }
 }
