cc5bf7285caf53b7258d23f851e055ddb793bf59
Adam Bourn <abourn@uw.edu>
2017-09-26 11:29:22 -0700

    setup for fixing skills redundancy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as ProjectsConst from './projects.js';
+var classNames = require('classnames');
 
 class App extends Component {
 
@@ -11,13 +12,13 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <section className="about">
+        <section id="about">
           <div>
             <header>
               <h1>Adam Bourn</h1>
               <nav>
-                <a>About</a>
-                <a href="#projectsSection">Projects</a>
+                <a href="#projects">Projects</a>
+                <a href="#skills">Skills</a>
                 <a href="https://drive.google.com/file/d/0B8Mc9nigpockVUR1QkE3UVcyX00/view?usp=sharing" target="_blank">Resume</a>
               </nav>
             </header>
@@ -26,15 +27,70 @@ class App extends Component {
             <p>Senior | Informatics | University of Washington</p>
           </div>
           <div className="arrowContainer">
-            <a href="#projectsSection"><i className="fa fa-angle-down fa-3x faa-passing animated"></i></a>
+            <a href="#projects"><i className="fa fa-angle-down fa-3x faa-passing animated"></i></a>
           </div>
         </section>
-        <section id="projectsSection" className="projects">
+        <section id="projects">
           <h2>Projects</h2>
           <div className="flex-container container">
               {projectsArray}
           </div>
         </section>
+        <section id="skills">
+          <h2>Skills</h2>
+
+
+          <div>
+            <i className="devicon-html5-plain "></i>
+            <p>HTML</p>
+          </div>
+          <div>
+            <i className="devicon-css3-plain "></i>
+            <p>CSS</p>
+          </div>
+          <div>
+            <i className="devicon-javascript-plain "></i>
+            <p>Javacript</p>
+          </div>
+          <div>
+            <i className="devicon-react-original "></i>
+            <p>React</p>
+          </div>
+          <div>
+            <i className="devicon-jquery-plain "></i>
+            <p>jQuery</p>
+          </div>
+          <div>
+            <i className="devicon-bootstrap-plain "></i>
+            <p>Bootstrap</p>
+          </div>
+          <div>
+            <i className="devicon-android-plain "></i>
+            <p>Android</p>
+          </div>
+          <div>
+            <i className="devicon-java-plain "></i>
+            <p>Java</p>
+          </div>
+          <div>
+            <i className="devicon-go-plain "></i>
+            <p>Go</p>
+          </div>
+          <div>
+            <i className="devicon-git-plain "></i>
+            <p>git</p>
+          </div>
+          <div>
+            <i className="devicon-github-plain "></i>
+            <p>GitHub</p>
+          </div>
+
+
+
+
+        </section>
+        <footer>
+        </footer>
       </div>
     );
   }
@@ -62,11 +118,9 @@ class Project extends Component {
             {content}
             <hr></hr>
             <p>{this.props.description} </p>
-
           </div>
         </div>
       </div>
-
     );
   }
 }
